c53641635e04678d0e0f4c87a6056215c1f029b3
yuya-h-29 <tzou11291996@gmail.com>
2020-08-14 18:03:49 +0900

    Initial Commit

---
+++ tic-tac-toe game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tic-tac-toe game
+//
+//  Created by Yuya Harada on 2020/08/14.
+//  Copyright © 2020 Yuya Harada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
